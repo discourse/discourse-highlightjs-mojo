03b15fb111083b6f74651eedeb8374789c5f8fc6
Rafael Silva <xfalcox@gmail.com>
2025-02-10 17:06:59 -0300

    Create component

---
+++ javascripts/discourse/initializers/mojo.js
@@ -0,0 +1,12 @@
+import $ from "jquery";
+import { withPluginApi } from "discourse/lib/plugin-api";
+import mojo from "../lib/mojo-highlightjs";
+
+export default {
+  name: "highlightjs-mojo",
+  initialize() {
+    withPluginApi((api) => {
+      api.registerHighlightJSLanguage("mojo", mojo);
+    });
+  },
+};
